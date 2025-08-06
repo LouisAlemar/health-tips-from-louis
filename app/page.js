49dacf7dc34fdf4bcba384151492f551ae33842c
Louis Alemar <louisalemar@gmail.com>
2025-08-06 00:25:18 -0400

    clean up code

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Home Page</h1>
-    </div>
+    <main className="container mx-auto min-h-screen max-w-3xl p-8">
+      <h1 className="text-4xl font-bold mb-8 text-center">Home Page</h1>
+    </main>
   );
 }
